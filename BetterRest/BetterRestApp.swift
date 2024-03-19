dd8f6b2b2e1f9f23a3430c7ffea14e7ea8121cbf
Marcus Benoit <benoit.marcus@gmail.com>
2024-03-19 17:47:35 +0100

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Marcus Benoit on 19.03.24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
